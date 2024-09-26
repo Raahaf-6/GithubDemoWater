2e7c0e26c6b5f61c83608b4a3ae2c46e04fafd62
Raahaf-6 <raahafy@gmail.com>
2024-09-26 12:07:08 +0300

    sss

--- GithubDemoWater/ContentView.swift
+++ GithubDemoWater/ContentView.swift
@@ -8,16 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var off = false
+    @State private var offf = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+         
+            Text("Water tracker 💦sss").bold()
+            Toggle("   Apple Health", isOn: $off)
+            Text("")
+            Stepper("   Cups To Drink Per Day \(offf)", value: $offf)
+              
+            Button("Continue") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            }
         .padding()
+        }
+    
+
+        
     }
-}
+
+
 
 #Preview {
     ContentView()
