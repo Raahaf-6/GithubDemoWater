aef68a570e0bda747841e398dbb240c0eee2aa8b
Raahaf-6 <raahafy@gmail.com>
2024-09-26 12:01:46 +0300

    Initial Commit

---
+++ GithubDemoWater/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GithubDemoWater
+//
+//  Created by Rahaf on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GithubDemoWater/GithubDemoWaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemoWaterApp.swift
+//  GithubDemoWater
+//
+//  Created by Rahaf on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemoWaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
